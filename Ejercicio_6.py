3ff4c2e5fe5c067391b65d6f3e103cc204b2c46e
alicecastellon <alicecastellon25@gmail.com>
2025-02-18 13:49:44 -0600

    primera rama

---
+++ Ejercicio_6.py
@@ -0,0 +1,32 @@
+#Declara una variable con un numero entero, otra con un numero decimal y otra con una cadena de texto.Muestra en pantalla el valor de cada una junto con su tipo de dato
+
+nombre_completo="bryan_hernandez_molina"
+ciudad="Managua"
+pais="nicaragua"
+edad_1=30
+altura_1=1.78
+estudia=False
+
+print(f"su nombre es {nombre_completo}")
+print(f"vive en {ciudad}")
+print(f"su pais es {pais}")
+print(f" tiene la edad de {edad_1}")
+print(f"su altura es {altura_1}")
+print(f"el estudia {estudia}")
+
+nombre_completo="elia_membreño_molina"
+ciudad="Managua"
+pais="nicaragua"
+edad_2=25
+altura_2=1.65
+estudia=True
+
+print(f"su nombre es {nombre_completo}")
+print(f"vive en {ciudad}")
+print(f"su pais es {pais}")
+print(f" tiene la edad de {edad_2}")
+print(f"su altura es {altura_2}")
+print(f"el estudia {estudia}")
+
+print(edad_1<edad_2)
+print(altura_1<altura_2)
