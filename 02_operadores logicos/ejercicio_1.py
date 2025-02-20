fd9dc6308b6b89147600d3bf11e3bcc230673a8c
alicecastellon <alicecastellon25@gmail.com>
2025-02-20 15:43:21 -0600

    nuevos ejercicios

--- 02_operadores logicos/ejercicio_1.py
+++ 02_operadores logicos/ejercicio_1.py
@@ -3,8 +3,9 @@
 #60-69 regular, 70-79 bueno, 80-89 muy bueno,
 #90-100 excelente
 
-calificacion_1= 0-59
-calificacion_2= 60-69
-calificacion_3= 70-79
-calificacion_4= 80-89
-calificacion_5= 90-100
+calificacion_1= 20
+calificacion_2= 56
+calificacion_3= 89
+calificacion_4= 86
+calificacion_5= 94
+
---
+++ Metodos_de_cadena/manipulacion1.py
@@ -0,0 +1,5 @@
+nombre='jose'
+print(f'Antes de aplicar el metodo upper: {nombre}')
+
+nombre1={nombre.upper()} 
+print(f"Jose despues de usar el meotodo de upper: {nombre.upper()} ")
